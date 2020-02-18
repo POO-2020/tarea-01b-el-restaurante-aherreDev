b1ca49686f0a3c8764827b4cbf76797cc80eef9a
AherreDev <aherredev@gmail.com>
2020-02-17 18:51:10 -0600

    [Add] completed Time model

--- models/Tiempo.model.js
+++ models/Tiempo.model.js
@@ -2,13 +2,28 @@ class Tiempo {
   hora;
   minuto;
   periodo;
+  /**
+   * @param {number} hora
+   * @param {number} minuto
+   * @param {string} periodo
+   */
   constructor(hora, minuto, periodo) {
     this.hora = hora;
     this.minuto = minuto;
     this.periodo = periodo;
   }
-  getFormato24Horas = () => {};
-  getFormato12Horas = () => {};
+  getFormato24Horas = () => {
+    let format;
+    if (this.periodo === "AM") {
+      format = `${this.hora}:${this.minuto}`;
+    } else {
+      format = `${this.hora + 12}:${this.minuto}`;
+    }
+    return format;
+  };
+  getFormato12Horas = () => {
+    return `${this.hora}:${this.minuto} ${this.periodo}`;
+  };
 }
 
 export default Tiempo;
